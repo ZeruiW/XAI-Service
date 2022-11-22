4ee10430c05ad08228fcc682270d7620af140f85
Anh Nguyen <aaanh@users.noreply.github.com>
2022-11-22 18:29:21 -0500

    update title tentatively

--- frontend/layouts/dashboard.js
+++ frontend/layouts/dashboard.js
@@ -7,7 +7,7 @@ export default function DashboardLayout({ children }) {
 		<div className="w-screen">
 			<div className="bg-green-300 text-center p-2 flex items-center">
 				<Image className="p-2" alt="sac-logo" src={logo} height={50} width={70}></Image>
-				<p className="p-2 font-bold">eXplainable AI Evaluation Service</p>
+				<p className="p-2 font-bold">eXplainable AI Evaluation Service - Demonstration - Proof of Concept</p>
 			</div>
 			<div>{children}</div>
 		</div>

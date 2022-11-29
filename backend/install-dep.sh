32bce824f8a5b922e1bb9195ea6a2114762bc4d3
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-11-28 23:55:05 -0500

    change db_service requirements

--- backend/install-dep.sh
+++ backend/install-dep.sh
@@ -2,12 +2,7 @@
 
 pip install -r ./requirements.txt &&
 
-conda install --file db_service/requirements.txt &&
-conda install --file evaluation_service/requirements.txt &&
-conda install --file model_service/requirements.txt &&
-conda install --file xai_service/requirements.txt &&
-
 pip install -r db_service/requirements.txt &&
 pip install -r evaluation_service/requirements.txt &&
 pip install -r model_service/requirements.txt &&
-pip install -r xai_service/requirements.txt &&
+pip install -r xai_service/requirements.txt

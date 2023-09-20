e76261471e315e54cb983c8a7101fed5129ee59f
ZeruiW <wangzerui418@gmail.com>
2023-09-19 22:18:59 -0400

    model hf

---
+++ backend/model_service/hf_imclass/model.py
@@ -0,0 +1,40 @@
+import io
+from flask import (
+    request, jsonify
+)
+from xai_backend_central_dev.flask_manager import ExecutorBluePrint
+import requests
+from dotenv import load_dotenv
+import os
+
+# 加载.env文件
+load_dotenv("config.env")
+
+
+ebp = ExecutorBluePrint(
+    'hf_imclass', __name__, component_path=__file__, url_prefix='/hf_imclass')
+
+def query(image_bytes):
+    response = requests.post(API_URL, headers=headers, data=image_bytes)
+    json_response = response.json()
+    print(json_response)  # 打印响应以供调试
+    return json_response
+
+
+@ebp.route('/', methods=['POST'])
+def pred():
+    if request.method == 'POST':
+        files = request.files
+        imgs = files.getlist('image')
+        file_name = [img.filename for img in imgs]
+        imgs = [i.read() for i in imgs]
+
+        rs = {}
+        for i in range(len(file_name)):
+            prediction = query(imgs[i])
+            scores = [round(item['score'], 6) for item in prediction]
+            labels = [item['label'] for item in prediction]
+            rs[file_name[i]] = {'scores': scores, 'labels': labels}
+
+        return jsonify(rs)
+

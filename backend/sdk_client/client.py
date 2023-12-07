d1d960b1f8b8c971dead7c9440c41727b5fa5636
hari-vinayak <harivinayak113@gmail.com>
2023-12-07 06:10:35 -0500

    1.Splitting the SDK into different appopriate classes. 2. Created a centralized interface class for the SDK which will encapsulate the functionalities of the different manager classes into a single class. This class, SDKInterface, will provide a unified API for various operations, reducing the complexity for the end user. 3. Made changes to the process of reading the configuration file from a)a file path b)from a string to handle the configuration content sent by the FE to process during the runtime.

--- backend/sdk_client/client.py
+++ backend/sdk_client/client.py
@@ -1,48 +1,81 @@
-from xai_sdk import TaskPublisherClient
+import sys
+sys.path.append('/Users/harivinayak/Developer/Research_Project/my-sdk/XAI-Service/backend')
 
-sdk = TaskPublisherClient(base_url="http://publisher_endpoint_url")
+
+#from xai_sdk.xai_sdk import PublisherManager, TaskSheetManager, TaskManager, PipelineManager, ConfigBasedManager
+
+from xai_sdk.xai_sdk.sdk_interface import SDKInterface
+
+base_url = "http://35.185.126.177:5006"
+
+sdk = SDKInterface(base_url)
+
+# # Create instances of each manager
+# publisher_manager = PublisherManager(base_url)
+# task_sheet_manager = TaskSheetManager(base_url)
+# task_manager = TaskManager(base_url)
+# pipeline_manager = PipelineManager(base_url)
+
+# # Create an instance of ConfigBasedManager with references to other managers
+# config_manager = ConfigBasedManager(base_url, publisher_manager, task_sheet_manager, task_manager, pipeline_manager)
+
+# File-based configuration example
+#config_file_path = "sdk_config_dev.yaml"
 
 # Activate Publisher
-#sdk.activate_publisher("http://publisher_endpoint_url")
+#publisher_manager.activate_publisher("http://35.185.126.177:5006")
+sdk.activate_publisher("http://35.185.126.177:5006")
 
 # Register Services
-#sdk.register_service_from_config()
+#config_manager.register_service_from_config(config_file_path)
+#sdk.register_service_from_config(config_file_path)
 
 # Get Registered services Info
+#publisher_manager.get_registered_services()
 #sdk.get_registered_services()
 
 # Create and Run Task Sheets
-#sdk.create_and_run_task_sheet_from_config()
+#config_manager.create_and_run_task_sheet_from_config(config_file_path)
+#sdk.create_and_run_task_sheet_from_config(config_file_path)
 
 # Get Task sheets Info
+#task_sheet_manager.fetch_task_sheet_info()
 #sdk.fetch_task_sheet_info()
 
-# Create and run pipelines paralelly
-#sdk.create_and_run_pipeline_from_config()
+yaml_config_string = """
+pipelines:
+  - name: "pipeline2"
+    xai_task_sheet_id: "8IU7NIAVYWJ0D0L"
+    evaluation_task_sheet_id: "VKNWMX6I9O2OVHF"
+"""
+
+# Create and run pipelines parallely
+#config_manager.create_and_run_pipeline_from_config(config_file_path)
+sdk.create_and_run_pipeline_from_config(yaml_config_string)
 
 # Get Pipelines Info
+#pipeline_manager.get_all_pipelines()
+
 #sdk.get_all_pipelines()
 
 # Delete pipeline
-#sdk.delete_pipeline("some_pipeline_id")
-
-
+#pipeline_manager.delete_pipeline("some_pipeline_id")
 
 #------------------------------Task Sheet Functions--------------------
 
 # Delete Task sheet
-#sdk.delete_task_sheet("some_task_sheet_id")
+#task_sheet_manager.delete_task_sheet("some_task_sheet_id")
 
 # GET Task Sheet by task_sheet_id
-#sdk.get_task_sheet_by_task_sheet_id(["some_task_sheet_id"])
+#task_sheet_manager.get_task_sheet_by_task_sheet_id(["some_task_sheet_id"])
 
 #----------------------------- Task functions----------------------
 
 # Get Task Result
-#sdk.task_result("nyEuJA8KvUnmhtn.6KGCOFN88Q")
+#task_manager.task_result("some_task_ticket_id")
 
 # Stop task
-#sdk.stop_task("some_task_ticket")
+#task_manager.stop_task("some_task_ticket")
 
 # Delete task
-#sdk.delete_task("some_task_ticket")
+#task_manager.delete_task("some_task_ticket")
